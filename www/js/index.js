43bdaa7c6b7a8914986d1cfa090f10b108ba549c
Manuel Molino <manolo@iesvirgendelcarmen.com>
2017-01-30 11:00:00 +0100

    Añadida función colocar barcos.

--- www/js/index.js
+++ www/js/index.js
@@ -1,18 +1,51 @@
-/*jslint devel:true */
+/*jslint browser:true, devel:true */
+/*global $:false */
 
 // JSON con los barcos
-// posteriormente lo almacenamos en localStorage
-var barcos = [
-    {'f':2},
-    {'s':3},
-    {'b':3},
-    {'d':4},
-    {'p':5}
-];
+var barcos = null;
 
 // variable para almacenar el tablero de juego
 // la matriz del tablero
 var tablero = null;
+var filas = 8;
+var columnas = 8;
+
+/**
+ * Esta función responde al evento "ready" y carga la 
+ * configuración inicial de mi APP. Si no hay configuración,
+ * cargamos una configuración por defecto.
+*/
+$(document).ready(function(){
+    //¿hay localStorage disponible? (almacenamos la conf. ahí)
+    if (typeof(Storage) !== "undefined") {
+        barcos = JSON.parse(localStorage.getItem("barcos"));
+        
+        if (barcos === null) {
+            barcos = [
+                {tam:2, letra:'f', nombre:'fragata'},
+                {tam:3, letra:'b', nombre:'buque'},
+                {tam:3, letra:'s', nombre:'submarino'},
+                {tam:4, letra:'d', nombre:'destructor'},
+                {tam:5, letra:'p', nombre:'portaaviones'},
+            ];            localStorage.setItem("barcos",JSON.stringify(barcos));
+        }
+        
+        filas = parseInt(localStorage.getItem("filas"));
+        columnas = parseInt(localStorage.getItem("columnas"));
+        
+        if (isNaN(filas) || isNaN(columnas)) {
+            filas = 8;
+            columnas = 8;
+            localStorage.setItem("filas",8);
+            localStorage.setItem("columnas",8);
+        }
+        
+    } else { 
+        // NO hay localStorage, no podemos guardar 
+        // conf. ni información de las partidas (puntuaciones)
+        console.log("No tenemos LocalStorage");
+    }
+});
 
 /**
     Esta función crea una matriz (en JS es un 
@@ -105,8 +138,42 @@ function moneda(){
 */
 function colocarBarcos(matriz){
     //Compruebo que haya más de ocho filas y que la primera fila(igual a las demás) sean más de 8 columnas.
-    if(matriz.length < 8 || matriz[0].length < 8){
-        return null;
+    for (var i=0; i<barcos.length;i++){
+        var barco = barcos[i];
+        var libre;
+        do { 
+            // intento colocar el barco hasta 
+            // que encuentro espacio libre para él
+            libre=true;
+            var direccion = moneda();
+            if (direccion===0) { // horizontal 
+                var fila = dado(matriz.length);
+                var col = dado(matriz[fila].length-barco.tam);
+                for (var j=0; j<barco.tam;j++){
+                    if(matriz[fila][j+col]!='a') {
+                        libre=false;
+                    }
+                }
+                if (libre) {
+                   for (var j=0; j<barco.tam;j++){
+                        matriz[fila][j+col]=barco.letra;
+                   }
+                }
+            } else { // vertical
+                var fila = dado(matriz.length-barco.tam);
+                var col = dado(matriz[fila].length);
+                for (var j=0; j<barco.tam;j++){
+                    if(matriz[j+fila][col]!='a') {
+                        libre=false;
+                    }
+                }
+                if (libre) {
+                   for (var j=0; j<barco.tam;j++){
+                        matriz[j+fila][col]=barco.letra;
+                   }
+                }
+            }
+        } while (!libre);
     }
     
 }
@@ -115,11 +182,12 @@ function colocarBarcos(matriz){
 /**
 
 */
-function crearPartida(fil,col){
+function crearPartida(){
     // crear una matriz de fil x col
-    tablero = crearMatriz(fil,col);
+    tablero = crearMatriz(filas,columnas);
     // rellenar la matriz "a"
     inicializaMatriz('a',tablero);
+    colocarBarcos(tablero);
     // volcar la matriz a consola
     matriz2console(tablero);
 }
@@ -136,8 +204,4 @@ function crearPartida(fil,col){
     
     
     
-    
-    
-    
-    
-    
+        
